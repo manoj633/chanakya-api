eb867dd7ac159d2850d655afd72d50e3858af82a
Manoj <manoj09633@gmail.com>
2024-07-20 11:26:11 +0530

    Configured mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected successfully!");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.use((req, res, next) => {
   console.log(req.url);
@@ -19,6 +31,15 @@ app.use((err, req, res, next) => {
   });
 });
 
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDb disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDb connected!");
+});
+
 app.listen(8080, () => {
+  connect();
   console.log("connected to backend");
 });
