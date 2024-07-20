6ba7364bc53f966dacb4fbd9d5936e421e8e2189
Manoj <manoj09633@gmail.com>
2024-07-20 12:13:54 +0530

    fixed minor errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-import User from "../models/userModel";
-import { createError } from "../utils/error";
+import User from "../models/userModel.js";
+import { createError } from "../utils/error.js";
 import bcrypt from "bcryptjs";
 
 export const login = async (req, res, next) => {
